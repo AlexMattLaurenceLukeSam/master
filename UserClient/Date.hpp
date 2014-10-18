367eac2bc3f034d5809312600835a2ea07c952f4
torchcar <torchcar@yahoo.com.au>
2014-10-18 13:25:14 +1100

    Create Date.hpp

---
+++ UserClient/Date.hpp
@@ -0,0 +1,19 @@
+#ifndef DATE_H
+#define DATE_H
+
+Struct Date {
+  Date(int iday,
+    int imonth,
+    int iyear):
+    day(iday),
+    month(imonth),
+    year(iyear)
+    { }
+
+  bool compare(Data)
+
+int day{0};
+int month{0};
+int year{0};
+};
+#endif
