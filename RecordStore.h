52b7a4d6a25b4102c117dcf281015349199be8a2
Matthew Hollingsworth <matt_hollingsworth@hotmail.com>
2014-10-18 15:56:08 +1100

    Create RecordStore.h

---
+++ RecordStore.h
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include "mysql_connection.h"
+#include "mysql_driver.h"
+#include "MyRecord.h"
+
+class MyRecordStore {
+public:
+        MyRecordStore();
+
+        ~MyRecordStore();
+        // Delete - returns false if there wasn't a record with that key
+        bool deleteRecord(const char* key) throw (const char*);
+        // Exists - is there a record with a given key
+        bool exists(const char* key) throw (const char*);
+        // Put - insert/replace a record; returns false if failed
+        // (In this example, key is actually the same as name field in data record)
+        void put(const char* key, const MyRecord *data) throw (const char*);
+        // Get - get record with keyerror: Failed connect to github.com:443; Connection refused while accessing
+        MyRecord *get(const char*key) throw (const char*);
+
+        vector<MyRecord*> *getInRole(const char* role) throw (const char*);
+
+        void close();
+
+        std::vector,string> *allKeys();
+
+        bool isOK()
+        {
+                return !(this->invalid);
+        }
+
+private:
+        bool invalid;
+        sql::Connection *dbcon;
+        sql::Drive *driver;
+        MyRecordStore(const MyRecordStore& orig);
+        MyRecordStore& operator=(const MyRecordStore);
+
+        void recordToTables(const MyRecord *data);
+
+};
