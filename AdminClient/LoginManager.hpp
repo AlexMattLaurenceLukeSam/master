d8d2ac7e86c601666172ba2aa877af735d3a8916
torchcar <torchcar@yahoo.com.au>
2014-10-18 12:35:48 +1100

    header guard

--- AdminClient/LoginManager.hpp
+++ AdminClient/LoginManager.hpp
@@ -1,8 +1,11 @@
+#ifndef LOGIN_MANAGER_H
+#define LOGIN_MANAGER_H
 Struct LoginManager {
   LoginManager(bool iloggedIn) :
     loggedIn(iloggedIn)
     { }
   bool loggedIn;
   bool login(std::string, std::string)
-  logout()
+  void logout()
 };
+#endif
