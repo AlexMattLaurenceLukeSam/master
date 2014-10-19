c2b0495b0f928228dbdd37894f0e186f61e21256
Matthew Hollingsworth <matt_hollingsworth@hotmail.com>
2014-10-19 15:27:29 +1100

    Update Record.h

--- Record.h
+++ Record.h
@@ -1,151 +1,63 @@
-*
- * File:        MyRecord.cpp
+/*
+ * File:        MyRecord.h
  * Author:      nabg
- * 
- * Created on 23 November 2012, 1:25 PM
- * 
- * See MyRecord.h for some explanation!
- * 
- * class MyRecord {
-public:
-    explicit MyRecord(string& id); 
-    void setName(string& aname) throw (MyException);
-    void setEmail(string& amail) throw (MyException);
-    void setInfo(string& info) throw (MyException);
-    void setImage(string& imagestr) throw (MyException);
-    void addRole(string& anotherrole) throw (MyException);
-    void addKeyValue(string& collectionname, string& key, string& value) throw(MyException);
-
-    string getID() const { return this->id; }
-    string getName() const { return this->name; }
-    string getEmail() const { return this->email; }
-    string getInfo() const { return this->info; }
-    string getImage() const { return this->image; }
-    string getAttribute(string& collectionname, string& key) const throw(MyException);
-    vector<string> getRoles() const { return this->roles; }
-    map<string,string> getPhones() const { return this->phones; }
-    map<string,string> getAddresses() const { return this->addresses; }
-    map<string,string> getOtherKV() const { return this->other; }
-    
-    bool hasRole(string& queryrole) const;
-    
-private:
-    string id; 
-    string name; 
-    string email; 
-    string image;
-    string info;
-    vector<string> roles;
-    map<string,string> phones;
-    map<string,string> addresses;
-    map<string,string> other;
-
-
-
- * 
- * 
+ *
+ * Created on 27 November 2012, 11:18 AM
  */
 
+#ifndef MYRECORD_H
+#define MYRECORD_H
+#include "MyException.h"
+#include <map>
+#include <string>
 #include <vector>
-#include <algorithm>
-#include "MyRecord.h"
-#include "stringtrimmer.h"
+using namespace std;
 
-//#include <boost/regex.hpp>
+class MyRecord {
+public:
+    explicit MyRecord(std::string& id); // I don't want implicit conversion from std::std::string to MyRecord!
+    // Mutator functions - setting of other fields
+    // In this version, hardly any of the implementations will actually throw exceptions
+    // later validation functions will be added
+    void setName(std::string& aname) throw (MyException);
+    void setEmail(std::string& amail) throw (MyException);
+    void setInfo(std::string& info) throw (MyException);
+    void setImage(std::string& imagestr) throw (MyException);
+    void addRole(std::string& anotherrole) ;
+    void removeRole(std::string& oldrole);
+    void addKeyValue(std::string& collectionname, std::string& key, std::string& value) throw(MyException);
+    // Accessor functions
 
-MyRecord::MyRecord(string& id) {
-    this->id = id;
-}
+    std::string getID() const { return this->id; }
+    std::string getName() const { return this->name; }
+    std::string getEmail() const { return this->email; }
+    std::string getInfo() const { return this->info; }
+    std::string getImage() const { return this->image; }
+    std::string getAttribute(std::string& collectionname, std::string& key) const throw(MyException);
+    const std::vector<std::string>& getRoles() const { return this->roles; }
+    const std::map<std::string,std::string>& getPhones() const { return this->phones; }
+    const std::map<std::string,std::string>& getAddresses() const { return this->addresses; }
+    const std::map<std::string,std::string>& getOtherKV() const { return this->other; }
 
-void MyRecord::setName(string& aname) throw (MyException) {
-    this->name = aname;
-}
+    bool hasRole(std::string& queryrole) const;
 
-void MyRecord::setEmail(string& amail) throw (MyException) {
- //   boost::regex validationExpression("^[a-z0-9_\\+-]+(\\.[a-z0-9_\\+-]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*\\.([a-z]{2,4})$");
- //   amail = trim(amail);
- //   boost::match_results<std::string::const_iterator> results;
- //   if (boost::regex_match(amail, results, validationExpression))
- //   {
-        this->email = amail;
- //   }
- //   else
- //   {
- //       string msg = "Email of wrong format";
- //       throw MyException(msg);
- //   }
-}
+private:
+    std::string id; // Also known as "nickname", or even "primary key"
+    std::string name; // full name
+    std::string email;
+    std::string image;
+    std::string info;
+    std::vector<std::string> roles;
+    std::map<std::string,std::string> phones;
+    std::map<std::string,std::string> addresses;
+    std::map<std::string,std::string> other;
+     // Finally, note that copy constructor and assignment operator are
+    // private (and no implementations will be defined).  I choose to 
+    // disallow such operations.
+    MyRecord(const MyRecord& orig);
+    MyRecord& operator=(const MyRecord&);
+    // Further there is no virtual destructor, I do not intend MyRecord
+    // to be the base class in some hierarchy.
+};
 
-void MyRecord::setInfo(string& info) throw (MyException) {
-    this->info = info;
-}
-
-void MyRecord::setImage(string& imagestr) throw (MyException) {
-    this->image = imagestr;
-}
-
-void MyRecord::addRole(string& anotherrole)  {
-    // Should it be an error to specify the same role more than once? No, it
-    // will not really matter - so don't throw exception, just don't duplicate.
-    vector<string>::iterator it;
-    it = find(this->roles.begin(), this->roles.end(), anotherrole);
-    if (it != this->roles.end()) return; // Found it, so don't duplicate
-    roles.push_back(anotherrole);
-}
-
-void MyRecord::removeRole(std::string& oldrole)
-{
-    this->roles.erase(remove(this->roles.begin(),
-        this->roles.end(), oldrole),
-        this->roles.end());
-}
-
-void MyRecord::addKeyValue(string& collectionname, string& key, string& value) throw (MyException) {
-    if (key.empty() || value.empty()) {
-        string msg = "Empty key or value";
-        throw MyException(msg);
-    }
-    // If same collection/key combination specified more than once, later value
-    // overwrites earlier value for that key.
-
-// Change to allow of rSTL's insert() not allowing for replacement
-    if (collectionname == "Phones") {
-        this->phones[key]=value;
-    } else
-        if (collectionname == "Addresses") {
-        this->addresses[key]=value;
-    } else
-        if (collectionname == "Other") {
-         this->other[key]=value;
-    } else {
-        string errmsg = "There is no collection called " + collectionname;
-        throw MyException(errmsg);
-    }
-}
-
-string MyRecord::getAttribute(string& collectionname, string& key) const throw (MyException) {
-
-
-    if (collectionname == "Phones") {
-        return phones.find(key)->second;
-    } else
-        if (collectionname == "Addresses") {
-        return addresses.find(key)->second;
-    } else
-        if (collectionname == "Other") {
-        return other.find(key)->second;
-    }
-
-    string errmsg = "There is no collection called " + collectionname;
-    throw MyException(errmsg);
-
-}
-
-
-bool MyRecord::hasRole(string&  anotherrole) const  {
-    // Should it be an error to specify the same role more than once? No, it
-    // will not really matter - so don't throw exception, just don't duplicate.
-    vector<string>::const_iterator it;
-    it = find(this->roles.begin(), this->roles.end(), anotherrole);
-   return(it != this->roles.end()) ;
-}
+#endif  /* MYRECORD_H */
