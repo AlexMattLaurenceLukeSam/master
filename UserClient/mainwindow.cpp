190cb7a2b177fcc80332567cbe8ed463f6a9eda3
7xe <evans.samuel7@gmail.com>
2014-10-25 10:34:31 +1100

    Update mainwindow.cpp

--- UserClient/mainwindow.cpp
+++ UserClient/mainwindow.cpp
@@ -1,5 +1,5 @@
-#include "mainwindow.hpp"
-#include "mainwindow.ui"
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -15,6 +15,30 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::login()
+{
+    std::string username = ui->usernameLogin->text().toStdString();
+    std::string password = ui->passwordLogin->text().toStdString();
+
+    if(username == "PCChair" && password == "admin")
+        setUser(3);
+    else if(username == "Reviewer" && password == "user")
+        setUser(2);
+    else if(username == "Author" && password == "user")
+        setUser(1);
+
+    //if(loginMgr.login(ui->usernameLogin->text(), ui->passwordLogin->text()))
+      //  setUser(loginMgr.currentUser->uType);
+
+
+}
+
+void MainWindow::logout()
+{
+    setUser(0);
+    //loginMgr.logout();
+}
+
 void MainWindow::setUser(int userType)
 {
     switch(userType)
@@ -23,12 +47,11 @@ void MainWindow::setUser(int userType)
         ui->tabWidget->clear();
         ui->tabWidget->addTab(ui->loginTab, "Login");
         break;
-    case 1://pcchair
-        ui->tableWidget->clear();
-        ui->tabWidget->addTab(ui->infoTabChair, "Information");
-        ui->tabWidget->addTab(ui->usersTab, "User Management");
-        ui->tabWidget->addTab(ui->papersTab, "Paper Management");
-        ui->tabWidget->removeTab(0);
+    case 1://author
+        ui->tabWidget->clear();
+        ui->tabWidget->addTab(ui->infoTabAuthor, "Information");
+        ui->tabWidget->addTab(ui->authorTab, "Author");
+        //ui->tabWidget->removeTab(0);
         break;
     case 2://reviewer
         ui->tableWidget->clear();
@@ -37,11 +60,12 @@ void MainWindow::setUser(int userType)
         ui->tabWidget->addTab(ui->reviewerTab, "Reviewer");
         ui->tabWidget->removeTab(0);
         break;
-    case 3://author
-        ui->tabWidget->clear();
-        ui->tabWidget->addTab(ui->infoTabAuthor, "Information");
-        ui->tabWidget->addTab(ui->authorTab, "Author");
-        //ui->tabWidget->removeTab(0);
+    case 3://pcchair
+        ui->tableWidget->clear();
+        ui->tabWidget->addTab(ui->infoTabChair, "Information");
+        ui->tabWidget->addTab(ui->usersTab, "User Management");
+        ui->tabWidget->addTab(ui->papersTab, "Paper Management");
+        ui->tabWidget->removeTab(0);
         break;
     }
 
@@ -49,12 +73,7 @@ void MainWindow::setUser(int userType)
 
 void MainWindow::on_login_clicked()
 {
-    if(username == "PCChair" && password == "admin")
-        setUser(1);
-    else if(username == "Reviewer" && password == "user")
-        setUser(2);
-    else if(username == "Author" && password == "user")
-        setUser(3);
+    login();
 }
 
 void MainWindow::on_quit_clicked()
@@ -62,22 +81,99 @@ void MainWindow::on_quit_clicked()
     qApp->quit();
 }
 
-void MainWindow::on_usernameLogin_editingFinished()
-{
-    username = ui->usernameLogin->text();
-}
-
-void MainWindow::on_passwordLogin_editingFinished()
-{
-    password = ui->passwordLogin->text();
-}
-
 void MainWindow::on_logout_clicked()
 {
-    setUser(0);
+    logout();
 }
 
 void MainWindow::on_logout_2_clicked()
 {
-    setUser(0);
+    logout();
+}
+
+void MainWindow::on_passwordLogin_returnPressed()
+{
+    login();
+}
+
+void MainWindow::on_createAccount_clicked()
+{
+    //if(loginMgr.createAccount(ui->usernameLogin->text(), ui->passwordLogin->text()))
+    //    setUser(loginMgr.currentUser->uType);
+}
+
+void MainWindow::on_apply_clicked()
+{
+    //send user details to server
+}
+
+void MainWindow::on_apply_2_clicked()
+{
+    //send user details to server
+}
+
+void MainWindow::on_addConfKey_clicked()
+{
+    ui->confKeyList->addItem(ui->confKeyEntry->text());
+}
+
+void MainWindow::on_rmvConfKey_clicked()
+{
+    //for(int i = 0; i < ui->confKeyList->selectedItems().size(); i++)
+    //    ui->confKeyList->(ui->confKeyList->selectedItems().at(i));
+    delete ui->confKeyList->currentItem();
+}
+
+void MainWindow::on_addAuthor_clicked()
+{
+    int row = ui->authorsTable->rowCount();
+    ui->authorsTable->insertRow(row);
+//    QTableWidgetItem item[4];
+//    item[0].setText(ui->newAuthorName->text());
+//    item[1].setText(ui->newAuthorEmail->text());
+//    item[2].setText(ui->newAuthorOrg->text());
+//    item[3].setText(ui->newAuthorPhone->text());
+//    ui->authorsTable->setItem(row, 0 , &item[0]);
+//    ui->authorsTable->setItem(row, 1 , &item[1]);
+//    ui->authorsTable->setItem(row, 2 , &item[2]);
+//    ui->authorsTable->setItem(row, 3 , &item[3]);
+}
+
+void MainWindow::on_rmvAuthor_clicked()
+{
+    ui->authorsTable->removeRow(ui->authorsTable->currentRow());
+    ui->authorsTable->clearSelection();
+}
+
+void MainWindow::on_addPaperKey_clicked()
+{
+    ui->paperKeyListAuth->addItem(ui->paperKeyEntry->text());
+}
+
+void MainWindow::on_rmvPaperKey_clicked()
+{
+    delete ui->paperKeyListAuth->currentItem();
+}
+
+void MainWindow::on_addAuthKey_clicked()
+{
+    ui->authKeyList->addItem(ui->authKeyEntry->text());
+}
+
+void MainWindow::on_rmvAuthKey_clicked()
+{
+    delete ui->authKeyList->currentItem();
+}
+
+void MainWindow::on_selectPaperAuthor_activated(int index)
+{
+
+}
+
+
+void MainWindow::on_selectPaperAuthor_currentTextChanged(const QString &arg1)
+{
+    ui->selectPaperAuthor->setItemText(ui->selectPaperAuthor->currentIndex(), ui->selectPaperAuthor->currentText());
+    if(ui->selectPaperAuthor->findText("*NEW*") == -1)
+        ui->selectPaperAuthor->addItem("*NEW*");
 }
