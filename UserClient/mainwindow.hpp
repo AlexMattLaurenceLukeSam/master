f7272e77c37b61b724515335d7955935a9c2ea3c
7xe <evans.samuel7@gmail.com>
2014-10-25 10:34:08 +1100

    Update mainwindow.hpp

--- UserClient/mainwindow.hpp
+++ UserClient/mainwindow.hpp
@@ -1,7 +1,8 @@
-#ifndef MAINWINDOW_HPP
-#define MAINWINDOW_HPP
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "LoginManager.hpp"
 
 namespace Ui {
 class MainWindow;
@@ -20,17 +21,45 @@ private slots:
 
     void on_quit_clicked();
 
-    void on_usernameLogin_editingFinished();
-
-    void on_passwordLogin_editingFinished();
-
     void on_logout_clicked();
 
     void on_logout_2_clicked();
 
+    void on_passwordLogin_returnPressed();
+
+    void on_createAccount_clicked();
+
+    void on_apply_clicked();
+
+    void on_apply_2_clicked();
+
+    void on_addConfKey_clicked();
+
+    void on_rmvConfKey_clicked();
+
+    void on_addAuthor_clicked();
+
+    void on_rmvAuthor_clicked();
+
+    void on_addPaperKey_clicked();
+
+    void on_rmvPaperKey_clicked();
+
+    void on_addAuthKey_clicked();
+
+    void on_rmvAuthKey_clicked();
+
+    void on_selectPaperAuthor_activated(int index);
+
+    void on_selectPaperAuthor_currentTextChanged(const QString &arg1);
+
 private:
-    Ui::MainWindow *ui;
     void setUser(int userType);
+    void login();
+    void logout();
+
+    Ui::MainWindow *ui;
+    LoginManager loginMgr;
     QString username;
     QString password;
 };
