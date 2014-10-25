5269871174b2ed3ff53dd01fbf47e11ff10a6380
7xe <evans.samuel7@gmail.com>
2014-10-25 15:10:36 +1100

    Create LoginManager.cpp

---
+++ UserClient/LoginManager.cpp
@@ -0,0 +1,49 @@
+#include "LoginManager.hpp"
+
+bool LoginManager::login(std::string username, std::string password)
+{
+//    UserType_t uType;//from packet
+
+//        if(username == "PCChair" && password == "admin")
+//            uType = AUTHOR;
+//        else if(username == "Reviewer" && password == "user")
+//            uType = REVIEWER;
+//        else if(username == "Author" && password == "user")
+//            uType = PCCHAIR;
+
+
+//    std::vector<std::string> keys;
+//    std::vector<PaperSummary> papers;
+//    std::string blank = "";
+//    if(uType == AUTHOR){
+//        currentUser = new Author(username, blank, blank, blank, blank, 0, AUTHOR);
+//        currentUser->setUserType(AUTHOR);
+//        return true;
+//    } else if (uType == REVIEWER){
+//        currentUser = new Reviewer(username, "", "", "", "", 0, REVIEWER);
+//        currentUser->setUserType(REVIEWER);
+//        return true;
+//    } else if (uType == PCCHAIR) {
+//        currentUser = new PCChair(username, "", "", "", "", 0, PCCHAIR);
+//        currentUser->setUserType(PCCHAIR);
+//        return true;
+//    } else return false;
+
+    //this function sends the username and password to the server for verification
+    //the response received is used to construct the currentUser pointer member of loginmanager
+
+
+
+}
+
+void LoginManager::logout()
+{
+    //no server client interaction
+    //acts as a destructor basically
+}
+
+bool LoginManager::createAccount(std::string username, std::string password)
+{
+    //this functions identically to login except that a new record is created in the database with the supplkied details
+    //rather than verifying that one exists with matching details
+}
