842e5d9a913dde89197f2386f10b7f710c2e64b8
torchcar <torchcar@yahoo.com.au>
2014-10-25 14:15:21 +1100

    Create PCChair.cpp

---
+++ UserClient/PCChair.cpp
@@ -0,0 +1,8 @@
+#include "PCChair.hpp"
+
+PCChair::checkConferencesDeadlines()
+{
+  //calls checkDeadlines in conferenceManager on server with activeConference* 
+  //need to use getActiveConference to obtain it
+  //returns nothing
+}
