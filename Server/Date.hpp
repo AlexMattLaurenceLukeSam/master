f441cb5a5358389b2dbaf5bea6aa24b0756b2393
torchcar <torchcar@yahoo.com.au>
2014-10-25 12:40:12 +1100

    made class

--- Server/Date.hpp
+++ Server/Date.hpp
@@ -1,34 +1,39 @@
 #ifndef DATE_H
 #define DATE_H
 
-struct Date {
-  Date(int iday,
-    int imonth,
-    int iyear):
-    day(iday),
-    month(imonth),
-    year(iyear)
-    { }
-  Date(const Date& in) {
-  day = in.day;
-  month = in.month;
-  year = in.year;}
+class Date {
+public:
+Date() { }
+Date(int iday,
+int imonth,
+int iyear):
+day(iday),
+month(imonth),
+year(iyear)
+{ }
+Date(const Date& in) {
+day = in.day;
+month = in.month;
+year = in.year;}
+int getDay(int iday) {day = iday;}
+int getMonth(int imonth) {month = imonth;}
+int getYear(int iyear) {year = iyear;}
 bool compare(const Date& idate) //returns true if idate is before or on the same day as this
-  {
-    if(idate.year != year) {
-      return idate.year < year;
-    } else {
-      if(idate.month != month) {
-        return idate.month < month;
-        } else {
-        if(idate.day != day) {
-      return idate.day < day;
-          }
-        }
-    }
-    return true;
-  }
-
+{
+if(idate.year != year) {
+return idate.year < year;
+} else {
+if(idate.month != month) {
+return idate.month < month;
+} else {
+if(idate.day != day) {
+return idate.day < day;
+}
+}
+}
+return true;
+}
+private:
 int day{0};
 int month{0};
 int year{0};
