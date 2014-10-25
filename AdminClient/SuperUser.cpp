84c9cffff3a72c9adf93a65ffba39b01bc03213a
torchcar <torchcar@yahoo.com.au>
2014-10-25 14:03:06 +1100

    Create SuperUser.cpp

---
+++ AdminClient/SuperUser.cpp
@@ -0,0 +1,8 @@
+#include "SuperUser.hpp"
+
+void SuperUser::checkAllDeadlines() 
+{
+  // returns nothing
+  // calls the checkAllDeadlines on the conf manager on the server
+  // takes no inputs
+}
