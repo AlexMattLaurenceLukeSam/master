be258818c74373eacbff5650b0a7c9529a25ab71
torchcar <torchcar@yahoo.com.au>
2014-10-25 11:35:29 +1100

    made class

--- AdminClient/LoginManager.hpp
+++ AdminClient/LoginManager.hpp
@@ -3,16 +3,24 @@
 
 #include "User.hpp"
 
-struct LoginManager {
+class LoginManager {
+public:
   LoginManager(bool iloggedIn) :
     loggedIn(iloggedIn)
     { }
   ~LoginManager() {
     delete currentUser;
   }
-  bool loggedIn;
+  bool getLoggedIn() {return loggedIn;}
+  User* getCurrentUser() {return currentUser;}
+  void setLoggedIn(bool iloggedIn) {loggedIn = iloggedIn;}
+  void setCurrentUser(User* icurrentUser) {currentUser = icurrentUser;}  
+  
   bool login(std::string, std::string);
   void logout();
+  
+private:
+  bool loggedIn;
   User* currentUser{nullptr};
 };
 #endif
