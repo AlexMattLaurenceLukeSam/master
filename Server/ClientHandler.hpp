35afe0953803aec8ce7d85911bcf1b0a85de7c95
torchcar <torchcar@yahoo.com.au>
2014-10-25 12:37:17 +1100

    made class

--- Server/ClientHandler.hpp
+++ Server/ClientHandler.hpp
@@ -5,15 +5,17 @@
 
 enum Request_t {ConferenceRequest, PaperRequest, UserRequest};
 
-struct ClientHandler {
+class ClientHandler {
+public:
   ClientHandler(Request_t irequest) : request(irequest)
     { }
-
-Request_t request;
-
-void handleClient();
-bool authenticate(std::string, std::string);
-bool connectToDB();
+  Request_t getRequest() {return request;}   
+  void setRequest(Request_t irequest) {request = irequest;}
+  void handleClient();
+  bool authenticate(std::string, std::string);
+  bool connectToDB();
+private:
+  Request_t request;
 
 };
 #endif
