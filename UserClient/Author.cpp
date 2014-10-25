43b3627be7f2e61ea9524c11a1c140b23a121dd5
7xe <evans.samuel7@gmail.com>
2014-10-25 15:05:54 +1100

    Create Author.cpp

---
+++ UserClient/Author.cpp
@@ -0,0 +1,28 @@
+#include "Author.hpp"
+
+void Author::getAllPapers()
+{
+    //sends request to server asking for PaperSummaries
+
+    //populates ownPaper vector with response
+
+
+}
+
+void Author::getPaper()
+{
+    //sends request to server asking for Paper with id matching the id of the current PaperSummary
+    //sets currentPaper equal to the Paper returned in the response
+}
+
+void Author::modifyDetails()
+{
+    //sends a modified User packet to the server
+    //no response
+}
+
+void Author::modifyPaper()
+{
+    //sends a modified Paper packet to the server
+    //no response
+}
