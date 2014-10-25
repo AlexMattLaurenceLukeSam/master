37011bf4c3cffb403956d29877a40098afb2eef9
torchcar <torchcar@yahoo.com.au>
2014-10-25 11:48:17 +1100

    made class

--- AdminClient/UserManager.hpp
+++ AdminClient/UserManager.hpp
@@ -2,17 +2,21 @@
 #define USER_MANAGER_H
 #include "User.hpp"
 
-struct UserManager {
+class UserManager {
+public:
   UserManager() { }
-  UserManager(User* itemp) : temp(itemp) { }
-  ~UserManager() { if(temp != nullptr)
-                     delete temp;}
-  User* temp{nullptr};
+  UserManager(User* icurrentUser) : currentUser(icurrentUser) { }
+  ~UserManager() { if(currentUser != nullptr)
+                     delete currentUser;}
+  User* getCurrentUser() {return currentUser;}
+  void setCurrentUser(User* icurrentUser) {currentUser = icurrentUser;}  
   void createUser();
   void removeUser();
   void listUsers();
   void viewUser();
   void editUser();
+private:  
+  User* currentUser{nullptr};
 
 };
 #endif
