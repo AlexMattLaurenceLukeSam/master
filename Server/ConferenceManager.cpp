e8c50bd3eed0cd047a80e523cb5d56211044f27c
torchcar <torchcar@yahoo.com.au>
2014-10-25 14:11:11 +1100

    Update ConferenceManager.cpp

--- Server/ConferenceManager.cpp
+++ Server/ConferenceManager.cpp
@@ -14,17 +14,20 @@
          << endl;
 }*/
 
-void ConferenceManager::checkDeadlines(const Conference& in)
+void ConferenceManager::checkDeadlines(Conference* in)
 {
+    if (in == nullptr) {
+        return;
+    }
     time_t t = time(0);   // get time now
     struct tm * now = localtime( & t );
     Date today(now->tm_year + 1900,
                now->tm_mon + 1,
                now->tm_mday);
-    in.isBeforePaperDeadline = in.paperDeadline.compare(today);
-    in.isBeforeSoftReviewDeadline = in.reviewDeadlineSoft.compare(today);
-    in.isBeforeHardReviewDeadline = in.reviewDeadlineHard.compare(today);
-    in.isBeforeDiscussDeadline = in.discussDeadline.compare(today);
+    in->isBeforePaperDeadline = in->paperDeadline.compare(today);
+    in->isBeforeSoftReviewDeadline = in->reviewDeadlineSoft.compare(today);
+    in->isBeforeHardReviewDeadline = in->reviewDeadlineHard.compare(today);
+    in->isBeforeDiscussDeadline = in->discussDeadline.compare(today);
 
 }
 
