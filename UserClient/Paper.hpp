34517ec707bb7f060558bad1f0d2caf5506de628
torchcar <torchcar@yahoo.com.au>
2014-10-26 16:59:04 +1100

    Update Paper.hpp

--- UserClient/Paper.hpp
+++ UserClient/Paper.hpp
@@ -15,6 +15,7 @@ class Paper {
       const std::vector<User>& iauthors,
       const std::vector<std::string>& ikeywords,
       const std::string& iconfKeyword,
+      const std::vector<Review>& ireviews,
       const Discussion& idiscussion,
       int ipaperID,
       int iconfID,
@@ -25,6 +26,7 @@ class Paper {
       authors(iauthors),
       keywords(ikeywords),
       confKeyword(iconfKeyword),
+      reviews(ireviews),
       discussion(idiscussion),
       paperID(ipaperID),
       confID(iconfID),
@@ -38,6 +40,7 @@ class Paper {
     std::vector<User> getAuthors() {return authors;}
     std::vector<std::string> getKeywords() {return keywords;}
     std::string getConfKeyword() {return confKeyword;}
+    std::vector<Review> getReviews() {return reviews;}
     Discussion getDiscussion() {return discussion;}
     int getPaperID() {return paperID;}
     int getConfID() {return confID;}
@@ -48,10 +51,11 @@ class Paper {
     void setAuthors(const std::vector<User>& iauthors) {authors = iauthors;}
     void setKeywords(const std::vector<std::string>& ikeywords) {keywords = ikeywords;}
     void setConfKeyWord(const std::string& iconfKeyword) {confKeyword = iconfKeyword;}
+    void setReviews(const std::Vector<Review>& ireviews) {reviews = ireviews;}
+    void setDiscussion(Discussion idiscussion) {discussion = idiscussion;} // potentially if the papers have the wrong discussions they can be swapped using this
     void setPaperID(int ipaperID) {paperID = ipaperID;}
     void setConfID(int iconfID) {confID = iconfID;}
     void setLeadAuthorID(int ileadAuthorID) {leadAuthorID = ileadAuthorID;}
-    void setDiscussion(Discussion idiscussion) {discussion = idiscussion;} // potentially if the papers have the wrong discussions they can be swapped using this
     void downloadPaper();
 
   private:
@@ -60,6 +64,7 @@ class Paper {
     std::vector<User> authors;
     std::vector<std::string> keywords;
     std::string confKeyword{""};
+    std::vector<Review> reviews;
     Discussion discussion;
     int paperID{0};
     int confID{0};
