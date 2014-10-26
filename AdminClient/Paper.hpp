8d72e483c266492f8a90b70c4e9543796410215d
torchcar <torchcar@yahoo.com.au>
2014-10-26 15:50:59 +1100

    Update Paper.hpp

--- AdminClient/Paper.hpp
+++ AdminClient/Paper.hpp
@@ -14,29 +14,38 @@ struct Paper {
     std::string abstract{""};
     std::vector<User> authors;
     std::vector<std::string> keywords;
+    std::string confKeyword{""};
     std::string fname{""};
     std::vector<Review> reviews;
     Discussion discussion;
     int paperID{0};
+    int confID{0};
+    int leadAuthorID{0};
 
     Paper() { }
     Paper(const std::string& ititle,
       const std::string& iabstract,
       const std::vector<User>& iauthors,
       const std::vector<std::string>& ikeywords,
+      const std::string& iconfKeyword,
       const std::string& ifname,
       const std::vector<Review>& ireviews,
       const Discussion& idiscussion,
-      int ipaperID)
+      int ipaperID,
+      int iconfID,
+      int ileadAuthorID)
       :
       title(ititle),
       abstract(iabstract),
       authors(iauthors),
       keywords(ikeywords),
+      confKeyword(iconfKeyword),
       fname(ifname),
       reviews(ireviews),
       discussion(idiscussion),
-      paperID(ipaperID)
+      paperID(ipaperID),
+      confID(iconfID),
+      leadAuthorID(ileadAuthorID)
       { }
 
     // no assignment operator or copy constructor deemed necessary at this point
