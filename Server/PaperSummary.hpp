544049d23f6ba9cfaa8ddc845b2c3a91edb87ef4
torchcar <torchcar@yahoo.com.au>
2014-10-26 15:12:37 +1100

    Create PaperSummary.hpp

---
+++ Server/PaperSummary.hpp
@@ -0,0 +1,11 @@
+#ifndef PAPER_SUMMARY_H
+#define PAPER_SUMMARY_H
+#include <string>
+struct PaperSummary {
+PaperSummary(int ipaperId, const std::string& ipaperName) :
+paperId(ipaperId), paperName(ipaperName)
+{ }
+int paperId{0};
+std::string paperName{""};
+};
+#endif
