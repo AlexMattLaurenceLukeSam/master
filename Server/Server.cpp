cfc673f4dceecffb699de1f9b267140663fec765
torchcar <torchcar@yahoo.com.au>
2014-10-26 14:11:49 +1100

    Update Server.cpp

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,23 +1,23 @@
 //NEW MAIN FOR SERVER
 
-#include "PaperManager.hpp"
 #include "ConferenceManager.hpp"
-
-
-
+#include "PaperManager.hpp"
+#include "UserManager.hpp"
 
 int main()
 {
   Database* db;
   PaperManager* pm;
   ConferenceManager* cm;
+  UserManager* um;
   
   
   db = new Database(/*CONSTRUCTOR*/);
   pm = new PaperManager(nullptr, db);
   cm = new ConferenceManager(std::vector<Conference*>(), db, nullptr);
+  um = new UserManager(nullptr, db);
   
-
+  delete um;
   delete cm;
   delete pm;
   delete db;
