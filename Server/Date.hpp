fd05284a3931e9bc53f1cad14678ac205c679587
laurencerm <l.robinsonmills@gmail.com>
2014-10-26 17:45:09 +1100

    made same as others

--- Server/Date.hpp
+++ Server/Date.hpp
@@ -1,44 +1,63 @@
 #ifndef DATE_H
 #define DATE_H
 
+#include <sstream>
+
 class Date {
+    
 public:
-Date() { }
-Date(int iday,
-int imonth,
-int iyear):
-day(iday),
-month(imonth),
-year(iyear)
-{ }
-Date(const Date& in) {
-day = in.day;
-month = in.month;
-year = in.year;}
-    int getDay() {return day;}
-    int getMonth() {return month;}
-    int getYear() {return year;}
+    Date() { }
+    Date(int iday,
+         int imonth,
+         int iyear):
+         day(iday),
+         month(imonth),
+         year(iyear)
+             { }
+    
+    Date(const Date& in)
+    {
+        day = in.day;
+        month = in.month;
+        year = in.year;
+    }
+    
+// NOTE: changed get functions, new below
     void setDay(int iday) {day = iday;}
     void setMonth(int imonth) {month = imonth;}
     void setYear(int iyear) {year = iyear;}
-bool compare(const Date& idate) //returns true if idate is before or on the same day as this
-{
-if(idate.year != year) {
-return idate.year < year;
-} else {
-if(idate.month != month) {
-return idate.month < month;
-} else {
-if(idate.day != day) {
-return idate.day < day;
-}
-}
-}
-return true;
-}
+    int getDay() {return day;}
+    int getMonth() {return month;}
+    int getYear() {return year;}
+    
+    bool compare(const Date& idate) //returns true if idate is before or on the same day as this
+    {
+        if(idate.year != year) {
+            return idate.year < year;
+        } else {
+            if(idate.month != month) {
+                return idate.month < month;
+            } else {
+                if(idate.day != day) {
+                    return idate.day < day;
+                }
+            }
+        }
+        return true;
+    }
+
+  std::string convertToString()
+  {
+    std::stringstream temp;
+    temp << day << "/" << month << "/" << year; 
+    return(temp.str());
+  } 
+    
 private:
-int day{0};
-int month{0};
-int year{0};
+    int day{0};
+    int month{0};
+    int year{0};
+    
 };
+
 #endif
