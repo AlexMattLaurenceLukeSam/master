368e6889eac6c807a28655c7688d746fbdf3a6f6
Matthew Hollingsworth <matt_hollingsworth@hotmail.com>
2014-10-26 17:14:02 +1100

    Update PaperSummary.hpp

--- Server/PaperSummary.hpp
+++ Server/PaperSummary.hpp
@@ -1,11 +1,16 @@
 #ifndef PAPER_SUMMARY_H
 #define PAPER_SUMMARY_H
+
 #include <string>
+
 struct PaperSummary {
-PaperSummary(int ipaperID, const std::string& ipaperName) :
-paperID(ipaperID), paperName(ipaperName)
-{ }
-int paperID{0};
-std::string paperName{""};
+	PaperSummary() {}
+
+	PaperSummary(int ipaperId, const std::string& ipaperName) :
+	paperId(ipaperId), paperName(ipaperName)
+	{ }
+
+	int paperId{0};
+	std::string paperName{""};
 };
 #endif
