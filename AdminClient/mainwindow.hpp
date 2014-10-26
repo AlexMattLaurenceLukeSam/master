5316a85a5b68c9b40c988de1c7869f4e9a8f7410
7xe <evans.samuel7@gmail.com>
2014-10-26 15:21:54 +1100

    Create mainwindow.hpp

---
+++ AdminClient/mainwindow.hpp
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_addConf_clicked();
+
+    void on_rmvConf_clicked();
+
+    void on_addUserKey_clicked();
+
+    void on_rmvUserKey_clicked();
+
+    void on_quit_2_clicked();
+    
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
