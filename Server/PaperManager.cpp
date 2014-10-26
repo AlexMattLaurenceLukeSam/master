5aa7f72b3515a1d83bd247b6eb5757a25130d317
torchcar <torchcar@yahoo.com.au>
2014-10-26 21:27:27 +1100

    Update PaperManager.cpp

--- Server/PaperManager.cpp
+++ Server/PaperManager.cpp
@@ -2,3 +2,10 @@ void PaperManager::fetchPaper(int paperID)
 {
   *currentPaper = db->fetchPaper(paperID);
 }
+
+PaperManager::sendAuthoredPaper(int paperID)
+{
+ fetchPaper(paperID);
+  //send Paper to client
+}
+
