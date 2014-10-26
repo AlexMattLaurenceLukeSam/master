f1bf3b650544b4935a6d522acff3ca2e5d171510
torchcar <torchcar@yahoo.com.au>
2014-10-26 21:35:35 +1100

    Update PaperManager.cpp

--- Server/PaperManager.cpp
+++ Server/PaperManager.cpp
@@ -3,9 +3,25 @@ void PaperManager::fetchPaper(int paperID)
   *currentPaper = db->fetchPaper(paperID);
 }
 
-PaperManager::sendAuthoredPaper(int paperID)
+void PaperManager::sendAuthoredPaper(int paperID)
 {
  fetchPaper(paperID);
   //send Paper to client
 }
 
+void PaperManager::sendPapersToReview(int userID)
+{
+ std::vector<int> paperIDsToReview = db->fetchPapersFromUserID(userID); //or something like that
+ std::vector<Paper> papersToReview;
+ for(unsigned int i = 0; i < paperIDsToReview.size(); ++i)
+ {
+   fetchPaper; //curentPaper now points to it
+   papersToReview[i] = *currentPaper;
+ }
+  //send this vector to the client
+}
+
+void PaperManager::sendPaper()
+{
+ //send *currentPaper to the client
+}
