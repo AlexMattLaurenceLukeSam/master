f046373faa5737bc958d3024a17a1ba3757db6a2
laurencerm <laurencerm@users.noreply.github.com>
2014-10-28 19:49:47 +1100

    changed to same as server/

--- UserClient/User.hpp
+++ UserClient/User.hpp
@@ -1,14 +1,13 @@
 #ifndef USER_H
 #define USER_H
 
-#include "../DataAll/Paper.hpp"
-
 #include <string>
+#include <vector>
 
 enum UserType_t {NOUSER, AUTHOR, REVIEWER, PCCHAIR, ADMIN};
 
-class User {
-public:
+struct User 
+{
     User()
     {
         password = "";
@@ -16,62 +15,63 @@ public:
         email = "";
         organisation = "";
         phone = "";
-        userID = 0;
+        userID = -1;
         userType = NOUSER;
-        currentPaper = Paper();
+//        currentPaper = Paper();
     }
-
-    User(const std::string& iuserName,
-        const std::string& ipassword,
-        const std::string& iname,
-        const std::string& iemail,
-        const std::string& iorganisation,
-        const std::string& iphone,
+    
+    User(std::string& iuserName,
+        std::string& iname,
+        std::string& iemail,
+        std::string& iorganisation,
+        std::string& iphone,
+        std::string& ipassword,
         int iuserID,
-        UserType_t iuserType)
+        std::vector<std::string>& ikeywords,
+        UserType_t iUserType)
     {
         userName = iuserName;
-        password = ipassword;
         name = iname;
         email = iemail;
         organisation = iorganisation;
         phone = iphone;
+        password = ipassword;
         userID = iuserID;
-        userType = iuserType;
+        keywords = ikeywords;
+        userType = iUserType;
+    }
+    
+    //no userID
+    User(const std::string& iuserName,
+        const std::string& iname,
+        const std::string& iemail,
+        const std::string& iorganisation,
+        const std::string& iphone,
+        const std::string& ipassword,
+        const std::vector<std::string>& ikeywords,
+        UserType_t iUserType)
+    {
+        userName = iuserName;
+        name = iname;
+        email = iemail;
+        organisation = iorganisation;
+        phone = iphone;
+        password = ipassword;
+        keywords = ikeywords;
+        userType = iUserType;
+        userID = -1;
     }
 
-    virtual ~User() {}
-    
-//    virtual void view() = 0;
-    virtual void modifyDetails();
-    void viewConferenceDetails();
-    int getUserID() {return userID;}
-    std::string getPassword() {return password;}
-    std::string getName() {return name;}
-    std::string getEmail() {return email;}
-    std::string getOrganisation() {return organisation;}
-    std::string getPhone() {return phone;}
-    UserType_t getUserType() {return userType;}
-    Paper getCurrentPaper() {return currentPaper;}
-    void setUserID(int iuserID) {userID = iuserID;}
-    void setPassword(const std::string& ipassword) {password = ipassword;}
-    void setName(const std::string& iname) {name = iname;}
-    void setEmail(const std::string& iemail) {email = iemail;}
-    void setOrganisation(const std::string& iorganisation) {organisation = iorganisation;}
-    void setPhone(const std::string& iphone) {phone = iphone;}
-    void setUserType(UserType_t iuserType) {userType = iuserType;}
-    void setCurrentPaper(const Paper& icurrentPaper) {currentPaper = icurrentPaper;}
-
     std::string userName;
-
-protected:
     std::string password;
     std::string name;
     std::string email;
     std::string organisation;
     std::string phone;
+    std::vector<std::string> keywords;
     int userID;
+    Paper currectPaper; // don't need this!
     UserType_t userType;
-    Paper currentPaper;
 };
 #endif
+
