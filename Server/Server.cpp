b06ffe2ee5a50d449b0fcb0ecc09c0bf9b12972a
laurencerm <l.robinsonmills+git@gmail.com>
2014-10-28 15:49:23 +1100

    removed networking from server

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -10,41 +10,20 @@
 
 int main(int argc, char *argv[])
 {
-//  Database* db;
-//  PaperManager* pm;
-//  ConferenceManager* cm;
-//  UserManager* um;
-//    
-//  db = new Database(/*CONSTRUCTOR*/);
-//  pm = new PaperManager(nullptr, db);
-//  cm = new ConferenceManager(std::vector<Conference*>(), db, nullptr);
-//  um = new UserManager(nullptr, db);
-  
+    Database* db;
+    PaperManager* pm;
+    ConferenceManager* cm;
+    UserManager* um;
 
-    std::cout << "Server application starting" << std::endl;
-    std::cout << "Initializing QT framework" << std::endl;
-    QApplication app(argc, argv);
-    std::cout << "Creating threaded network server" << std::endl;
-    ServerMain *theServer = new ServerMain();
-    std::cout << "Server location: " << theServer->getIpAddress().toStdString()
-              << ":" << theServer->listeningPort().toStdString() << std::endl;
-    if(!theServer->serverIsOK()) {
-        std::cout << "Failed" << std::endl;
-        std::exit(1);
-    }
-  
-    
-    // create and show your widgets here
-//    gDialogptr = new Dialog();
-//    return gDialogptr->exec();
-  
-    // GO! GO! GO!
-    int rtnValue = app.exec();
-   
-//  delete um;
-//  delete cm;
-//  delete pm;
-//  delete db;
+    db = new Database(/*CONSTRUCTOR*/);
+    pm = new PaperManager(NULL, db);
+    cm = new ConferenceManager(std::vector<Conference*>(), db, NULL);
+    um = new UserManager(NULL, db);
 
-    return rtnValue;
+    delete um;
+    delete cm;
+    delete pm;
+    delete db;
+
+    return 0;
 }
