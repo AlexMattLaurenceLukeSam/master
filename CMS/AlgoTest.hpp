6ebb80f856c6d6aade8fe18ccbfcbef342b6e8c1
torchcar <torchcar@yahoo.com.au>
2014-10-30 03:01:31 +1100

    Update AlgoTest.hpp

--- CMS/AlgoTest.hpp
+++ CMS/AlgoTest.hpp
@@ -1,1 +1,21 @@
+#ifndef ALGOTEST_HPP
+#define	ALGOTEST_HPP
 
+class AlgoTest : public CppUnit::TestFixture
+{
+    CPPUNIT_TEST_SUITE (AlgoTest);
+    CPPUNIT_TEST (testFindMinScore);
+    CPPUNIT_TEST (testSetNewReviewer);
+    CPPUNIT_TEST_SUITE_END ();
+private:
+PaperForReview a;
+public:
+	void setUp();
+	void tearDown();
+protected:
+void testFindMinScore();
+void testSetNewReviewer();
+
+};
+
+#endif
