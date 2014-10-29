ccd12902bc63504cecbc2f0aaa08802485bf2022
torchcar <torchcar@yahoo.com.au>
2014-10-29 23:49:33 +1100

    Create AlgoTest.cpp

---
+++ CMS/AlgoTest.cpp
@@ -0,0 +1,16 @@
+#include "AlgoTest.hpp"
+#include "Algo.hpp"
+
+CPPUNIT_TEST_SUITE_REGISTRATION (AlgoTest);
+
+void AlgoTest::setUp()
+{
+	// Put the dates back to standard state
+
+	
+}
+
+void AlgoTest::tearDown()
+{
+	// Nothing to do
+}
