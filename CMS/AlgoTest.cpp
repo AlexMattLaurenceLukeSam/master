3952d7c55b179771053e3c754c8563b886ba6c0d
torchcar <torchcar@yahoo.com.au>
2014-10-29 23:54:10 +1100

    Update AlgoTest.cpp

--- CMS/AlgoTest.cpp
+++ CMS/AlgoTest.cpp
@@ -1,16 +1,1 @@
-#include "AlgoTest.hpp"
-#include "Algo.hpp"
 
-CPPUNIT_TEST_SUITE_REGISTRATION (AlgoTest);
-
-void AlgoTest::setUp()
-{
-	// Put the dates back to standard state
-
-	
-}
-
-void AlgoTest::tearDown()
-{
-	// Nothing to do
-}
