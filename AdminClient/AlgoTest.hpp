3ceaf6550a11f9ca1ef728c90ac3f33f0c665546
torchcar <torchcar@yahoo.com.au>
2014-10-29 23:54:32 +1100

    Create AlgoTest.hpp

---
+++ AdminClient/AlgoTest.hpp
@@ -0,0 +1,32 @@
+#ifndef ALGO_HPP
+#define	ALGO_HPP
+
+#include <cppunit/TestCase.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include "Algo.hpp"
+
+class AlgoTest : public CppUnit::TestFixture
+{
+    CPPUNIT_TEST_SUITE (AlgoTest);
+    CPPUNIT_TEST (testMinScore);
+    CPPUNIT_TEST_SUITE_END ();
+private:
+PaperForReview a;
+public:
+	void setUp();
+	void tearDown();
+protected:
+void testMinScore();
+
+};
+/*
+ * File:   AlgoTest.hpp
+ * Author: torchcar
+ *
+ * Created on 26 February 2009, 1:20 PM
+ */
+
+
+
+
+#endif	/* ALGO_HPP */
