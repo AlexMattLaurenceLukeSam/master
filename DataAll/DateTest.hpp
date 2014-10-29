33168e84084c91a0118a5412bcf5dcf056cfe71b
torchcar <torchcar@yahoo.com.au>
2014-10-29 16:51:32 +1100

    Create DateTest.hpp

---
+++ DataAll/DateTest.hpp
@@ -0,0 +1,38 @@
+#ifndef _BITMAPTEST_HPP
+#define	_BITMAPTEST_HPP
+
+#include <cppunit/TestCase.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include "Date.hpp"
+
+class DateTest : public CppUnit::TestFixture
+{
+    CPPUNIT_TEST_SUITE (DateTest);
+    CPPUNIT_TEST (testEquals);
+    CPPUNIT_TEST (testSetBit);
+    CPPUNIT_TEST (testSetBitAndCount);
+    CPPUNIT_TEST_SUITE_END ();
+private:
+	Date a;
+	Date b;
+public:
+	void setUp();
+	void tearDown();
+protected:
+	void testEqualDates();
+	void testMonthAheadDayBehind();
+	void testYearAheadMonthBehind();
+	void testYearBehindDayAhead();
+
+};
+/*
+ * File:   DateTest.hpp
+ * Author: torchcar
+ *
+ * Created on 26 February 2009, 1:20 PM
+ */
+
+
+
+
+#endif	/* _BITMAPTEST_H */
